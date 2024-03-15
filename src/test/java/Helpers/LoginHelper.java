f7ed91261d54b49efca5b5b97d91ecaa62fd40a6
Rishabh Gungah <rishrcc@hotmail.com>
2024-03-15 14:18:48 +0400

    Remove extent reporting

--- src/test/java/Helpers/LoginHelper.java
+++ src/test/java/Helpers/LoginHelper.java
@@ -1,13 +1,12 @@
 package Helpers;
+
 import Pages.LoginPage;
 import Utility.BrowserDriver;
-import com.aventstack.extentreports.Status;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import static Tests.LoginTest.*;
 
 public class LoginHelper extends BrowserDriver {
 
@@ -18,13 +17,10 @@ public class LoginHelper extends BrowserDriver {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LoginPage.txtUsername)));
         driver.findElement(By.xpath(LoginPage.txtUsername)).sendKeys(username);
         driver.findElement(By.xpath(LoginPage.txtPassword)).sendKeys(password);
-        //test.log(Status.PASS,"Username and Password entered successfully");
     }
 
     public static void clickOnLogin()
     {
         driver.findElement(By.xpath(LoginPage.btnLogin)).click();
-        //test.log(Status.PASS,"Login button clicked on successfully");
     }
-
 }
